39ee570b3906c406fc6128e9ef95eef8c457aa58
riteshkusingh <ritesh@gmail.com>
2026-03-24 07:59:21 +0530

    allowed auth header

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ const applyCors = (req, res) => {
   }
 
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Max-Age", "86400");
 };
 
